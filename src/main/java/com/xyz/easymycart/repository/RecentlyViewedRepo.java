825ac07e47e15c5e7e807d6f9404ed91549922ae
Chandravadhan <m.chandravadhan@gmail.com>
2025-06-22 23:10:57 +0530

    written query for to fetch recently viewed products by userId and productId
    moved CORS configuration to filters and removed @CrossOrigin annotations

--- src/main/java/com/xyz/easymycart/repository/RecentlyViewedRepo.java
+++ src/main/java/com/xyz/easymycart/repository/RecentlyViewedRepo.java
@@ -1,22 +1,25 @@
 package com.xyz.easymycart.repository;
 
 import com.xyz.easymycart.model.RecentlyViewed;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface RecentlyViewedRepo extends JpaRepository<RecentlyViewed,Long> {
+public interface RecentlyViewedRepo extends JpaRepository<RecentlyViewed, Long> {
 
-   RecentlyViewed save(RecentlyViewed recentlyViewed);
+  RecentlyViewed save(RecentlyViewed recentlyViewed);
 
-   @Query(value = "select * from recently_viewed r where r.product_id = :productId and r.user_id = :userId",nativeQuery = true)
-   Optional<RecentlyViewed> findByUserIdAndProductId(@Param("productId") Long productId, @Param("userId") Long userId);
+  @Query(
+      value =
+          "select * from recently_viewed r where r.product_id = :productId and r.user_id = :userId",
+      nativeQuery = true)
+  Optional<RecentlyViewed> findByUserIdAndProductId(
+      @Param("productId") Long productId, @Param("userId") Long userId);
 
-   @Query(value = "select * from recently_viewed r where r.user_id = :userId",nativeQuery = true)
-   List<RecentlyViewed> getRecentlyViewedByUserId(@Param("userId") Long userId);
+  @Query(value = "select * from recently_viewed r where r.user_id = :userId", nativeQuery = true)
+  List<RecentlyViewed> getRecentlyViewedByUserId(@Param("userId") Long userId);
 }
