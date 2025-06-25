acd1f82e1372db8cfd570fc9c7a5c66463498a28
Chandravadhan <m.chandravadhan@gmail.com>
2025-06-25 22:19:48 +0530

    Added repository method to delete address by ID using native query
    moved CORS configuration to filters and removed @CrossOrigin annotations

--- src/main/java/com/xyz/easymycart/repository/AddressRepository.java
+++ src/main/java/com/xyz/easymycart/repository/AddressRepository.java
@@ -1,40 +1,45 @@
 package com.xyz.easymycart.repository;
 
 import com.xyz.easymycart.model.Address;
+import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-import java.util.List;
-
 @Repository
-public interface AddressRepository extends JpaRepository<Address,Long> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
 
-    Address save(Address address);
+  Address save(Address address);
 
-    @Query(value = "select * from address a where a.user_id = :userId",nativeQuery = true)
-    List<Address> findAddressByUserId(@Param("userId") Long userId);
+  @Query(value = "select * from address a where a.user_id = :userId", nativeQuery = true)
+  List<Address> findAddressByUserId(@Param("userId") Long userId);
 
-    @Query(value = "select * from address a where a.id = :Id",nativeQuery = true)
-    Address findAddressById(@Param("Id") Long Id);
+  @Query(value = "select * from address a where a.id = :Id", nativeQuery = true)
+  Address findAddressById(@Param("Id") Long Id);
 
-    @Modifying
-    @Transactional
-    @Query(value = "UPDATE address SET full_name = :fullName, phone = :phone, pin_code = :pinCode, flat_number = :flatNumber, Area = :area, village = :village, landmark = :landmark, district = :district, state = :state WHERE id = :id", nativeQuery = true)
-    void updateAddressById(
-            @Param("id") Long id,
-            @Param("fullName") String fullName,
-            @Param("phone") String phone,
-            @Param("pinCode") String pinCode,
-            @Param("flatNumber") String flatNumber,
-            @Param("area") String area,
-            @Param("village") String village,
-            @Param("landmark") String landmark,
-            @Param("district") String district,
-            @Param("state") String state
-    );
+  @Modifying
+  @Transactional
+  @Query(value = "delete from address a where a.id = :Id",nativeQuery = true)
+  void deleteAddressById(@Param("Id") Long Id);
 
+  @Modifying
+  @Transactional
+  @Query(
+      value =
+          "UPDATE address SET full_name = :fullName, phone = :phone, pin_code = :pinCode, flat_number = :flatNumber, Area = :area, village = :village, landmark = :landmark, district = :district, state = :state WHERE id = :id",
+      nativeQuery = true)
+  void updateAddressById(
+      @Param("id") Long id,
+      @Param("fullName") String fullName,
+      @Param("phone") String phone,
+      @Param("pinCode") String pinCode,
+      @Param("flatNumber") String flatNumber,
+      @Param("area") String area,
+      @Param("village") String village,
+      @Param("landmark") String landmark,
+      @Param("district") String district,
+      @Param("state") String state);
 }
